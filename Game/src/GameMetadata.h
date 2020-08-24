ddcd30e0e539cd481125cebac6bf28c3cb06e84e
Leo Durrant <poweredbykurisu@hotmail.com>
2020-08-24 13:15:03 +0100

    Made some fixes for Linux

--- Game/src/GameMetadata.h
+++ Game/src/GameMetadata.h
@@ -2,7 +2,7 @@
 
 // PURPOSE: Provides the Engine with metadata of the build.
 // GENERATED CODE --- CHANGED ON BUILD --- ANY MANUAL CHANGES WILL BE OVERWRITTEN.
-#define GAME_BUILD_NUMBER   53
-#define GAME_GIT_HASH      "cf100a9162585b30c569f6a355d3989909dffcae"
-#define GAME_BUILD_TIME     1598266475.7711995
-#define GAME_GIT_DESC      "iteration-3-84-gcf100a9"
+#define GAME_BUILD_NUMBER   60
+#define GAME_GIT_HASH      "1a8b5e9ea6860fbcc979a07291b113476603c70f"
+#define GAME_BUILD_TIME     1598270808.6724246
+#define GAME_GIT_DESC      "iteration-3-85-g1a8b5e9"
--- Launcher/launcher/main_linux.cpp
+++ Launcher/launcher/main_linux.cpp
@@ -8,14 +8,19 @@
 #include <unistd.h>
 #define MAX_PATH PATH_MAX
 
-typedef int (*Engine_t)(int argc, char** argv);
+typedef bool (*GameDLL_t)(int argc, char** argv);
+typedef int (*Engine_t)(int argc, char** argv, GameDLL_t pGameDLL);
 #define stringize(a) #a
 #define engine_binary(a,b,c) a stringize(b) c 
 
 extern "C" int LauncherMain(int argc, char** argv)
 {
 	const char* pBinaryName = engine_binary("bin/lib", MOD_NAME_L, ".so");
+	const char* pGameBinaryN = engine_binary("assets/bin/lib", GAME_NAME_L, ".so");
+	
 	void* engine = dlopen(pBinaryName, RTLD_NOW);
+	void* game = dlopen(pGameBinaryN , RTLD_NOW);
+	
 	int ret = 0;
 	
 	if (!engine)
@@ -23,14 +28,29 @@ extern "C" int LauncherMain(int argc, char** argv)
 		printf("Failed to open %s (%s)\n", pBinaryName, dlerror());
 		return -1;
 	}
+	
+	if (!game)
+	{
+		printf("Failed to open %s (%s)\n", pGameBinaryN, dlerror());
+		return -1;
+	}
+	
 	Engine_t main = (Engine_t)dlsym(engine, "Engine");
+	GameDLL_t mGame = (GameDLL_t)dlsym(game, "GameDLL");
+	
 	if (!main)
 	{
 		printf("Failed to find engine entry point (%s)\n", dlerror());
 		return -1;
 	}
+	
+	if (!mGame)
+	{
+		printf("Failed to find GameDLL entry point (%s)\n", dlerror());
+		return -1;
+	}
 
-	ret = main(argc, argv);
+	ret = main(argc, argv, mGame);
 	dlclose(engine);
 
 	return ret;
