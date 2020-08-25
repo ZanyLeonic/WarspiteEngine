3a5f26c5039dc9a3d2c4c6c471a9ceffd19cba34
Leo Durrant <poweredbykurisu@hotmail.com>
2020-08-25 22:14:38 +0100

    AAAAAAAAAAAAAAAAAAAAAA

--- build-deps-x86.ps1
+++ build-deps-x86.ps1
@@ -20,11 +20,11 @@ function GenerateProjectFiles() {
         SetEnv
     }
     $bargs = @( 
-    "-A Win32",
+    "-AWin32",
     "-DCMAKE_TOOLCHAIN_FILE=$VcpkgDir\scripts\buildsystems\vcpkg.cmake",
     '-DCMAKE_BUILD_TYPE="Debug"', 
     "-DVCPKG_TARGET_TRIPLET=x86-windows",
-	"-Bbuild"
+	"-Bbuild",
     ".")
     & cmake $bargs
 	
